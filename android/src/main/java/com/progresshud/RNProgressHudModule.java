422f2b5e0bda61a3c0b050d8dc6516370f56179d
BunHouth <bunhouth99@gmail.com>
2019-07-04 16:04:10 +0700

    androidX support

--- android/src/main/java/com/progresshud/RNProgressHudModule.java
+++ android/src/main/java/com/progresshud/RNProgressHudModule.java
@@ -2,8 +2,8 @@ package com.progresshud;
 
 import android.content.Context;
 import android.os.Handler;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import android.widget.ImageView;
 
 import com.facebook.react.bridge.ReactApplicationContext;
